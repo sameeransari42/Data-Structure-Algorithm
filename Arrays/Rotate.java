e49690fd8a9783607385fa02229eaff93a63185f
Sameer Ansari <ahmad4007k@gmail.com>
2024-12-25 01:42:34 +0530

    reduce the code of rotate array

--- Arrays/Rotate.java
+++ Arrays/Rotate.java
@@ -6,38 +6,20 @@ public class Rotate {
     public static void main(String[] args) {
         //Note: Hey, Sameer please reduce this
         int[] arr = {1, 2, 3, 4, 5, 6};
-        int idx = 4;
+        int idx = 3;
 
-        int k = 0;
-        int l = arr.length-1;
-
-        int i = 0;
-        int j = arr.length-idx -1;
-
-        while (k <= l) {
-            int temp = arr[k];
-            arr[k] = arr[l];
-            arr[l] = temp;
-            k++; l--;
-        }
-
-        while (i < j) {
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-            i++; j--;
-        }
-
-        int m = arr.length - idx;
-        int n = arr.length-1;
-
-        while (m <= n) {
-            int temp = arr[m];
-            arr[m] = arr[n];
-            arr[n] = temp;
-            m++; n--;
-        }
+        reverse(arr, 0, arr.length-1);
+        reverse(arr, 0, arr.length-idx-1);
+        reverse(arr, arr.length-idx, arr.length-1);
 
         System.out.println(Arrays.toString(arr));
+    }
+    public static void reverse(int[] arr, int a, int b) {
+        while (a <= b) {
+            int temp = arr[a];
+            arr[a] = arr[b];
+            arr[b] = temp;
+            a++; b--;
+        }
     }
 }
